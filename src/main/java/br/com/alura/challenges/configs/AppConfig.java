868aee5841c76a8cd366e4860b32734133de5e8e
Mauricio Mityo Hidani <128845494+mauricioHidani@users.noreply.github.com>
2024-09-23 09:23:33 -0300

    fix: ajuste de taxa de transferencia

--- src/main/java/br/com/alura/challenges/configs/AppConfig.java
+++ src/main/java/br/com/alura/challenges/configs/AppConfig.java
@@ -2,7 +2,7 @@ package br.com.alura.challenges.configs;
 
 public class AppConfig {
 
-	private final float TRANSFER_FEE = 0.12f;
+	private final float TRANSFER_FEE = 12.3f;
 	private final float OVERDRAFT_RATE = 0.30f;
 
 	public float getTransferFee() {
